137ffbbfcd08f9934f89617ca05957aaa86d69b9
William Kwon <williamkwon97@gmail.com>
2019-02-17 23:48:56 -0600

    flix part2

---
+++ Flix/flix2/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix2
+//
+//  Created by Wiliam Kwon on 2/17/19.
+//  Copyright © 2019 Wiliam Kwon. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
---
+++ Flix/flix2/SuperheroViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SuperheroViewController.swift
+//  flix2
+//
+//  Created by Wiliam Kwon on 2/17/19.
+//  Copyright © 2019 Wiliam Kwon. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class SuperheroViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    var movie: [String:Any]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        
+        synopsisLabel.sizeToFit()
+        
+        let baseURL="https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseURL + posterPath)
+        
+        posterView.af_setImage(withURL: posterUrl!)
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropUrl!)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
