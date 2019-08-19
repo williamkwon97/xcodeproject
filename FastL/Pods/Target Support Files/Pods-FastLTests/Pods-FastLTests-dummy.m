572e92ae92851cc9d936911321aed4d300ab4513
William Kwon <williamkwon97@gmail.com>
2019-08-19 09:27:45 -0500

    c

--- FastL/Pods/Target Support Files/Pods-FastLTests/Pods-FastLTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FastLTests : NSObject
-@end
-@implementation PodsDummy_Pods_FastLTests
-@end
--- FastL/Pods/Target Support Files/Pods-FastLUITests/Pods-FastLUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FastLUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FastLUITests
-@end
