98c1d1b7e49523d3f6715d60808ddcd7abe51e29
William Kwon <34716439+williamkwon97@users.noreply.github.com>
2020-02-05 14:16:09 -0600

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by William Kwon on 2/5/20.
+//  Copyright © 2020 William Kwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
